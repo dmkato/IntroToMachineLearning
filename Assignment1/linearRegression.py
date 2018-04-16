ecce4eeba3b38d43df7aae24310df7ec17c1e62a
dmkato <dmkato@gmail.com>
2018-04-15 19:56:25 -0700

    Added instructions to top of prog

--- Assignment1/linearRegression.py
+++ Assignment1/linearRegression.py
@@ -1,3 +1,10 @@
+# By Daniel Kato and Nathan Shepherd
+#
+# Instructions:
+#  1. Place data files directly in folder with python files
+#  2. Source the python3.5 venv by running `source /scratch/cs434spring2018/env_3.5/bin/activate`
+#  3. Run linear Regression with command `python3.5 linearRegression.py`
+
 from functools import reduce
 # import matplotlib.pyplot as plt
 import numpy as np
--- Assignment1/logisticRegression.py
+++ Assignment1/logisticRegression.py
@@ -1,3 +1,10 @@
+# By Daniel Kato and Nathan Shepherd
+#
+# Instructions:
+#  1. Place data files directly in folder with python files
+#  2. Source the python3.5 venv by running `source /scratch/cs434spring2018/env_3.5/bin/activate`
+#  3. Run logistic Regression with command `python3.5 logisticRegression.py`
+
 from functools import reduce
 # import matplotlib.pyplot as plt
 import numpy as np
