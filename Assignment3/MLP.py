6a7625e75c6c9ead31a6695408396e8ec84533a3
dmkato <dmkato@gmail.com>
2018-05-13 11:36:39 -0700

    Cleaned up MLP.py and added slides and report

--- Assignment3/MLP.py
+++ Assignment3/MLP.py
@@ -72,8 +72,9 @@ def show_examples():
         image = X_train[i,:,:,:].numpy()
         plt.imshow(image.reshape(3,32,32).transpose(1,2,0))
         plt.title('Class: '+str(y_train[i]))
+    plt.show()
 
-def plot():
+def plot_results():
     plt.figure(figsize=(5,3))
     plt.plot(np.arange(1,epochs+1), lossv)
     plt.title('validation loss')
@@ -83,22 +84,30 @@ def plot():
     plt.title('validation accuracy');
     plt.show()
 
+def create_train_loader():
+    loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10('./data', train=True, download=True,
+                transform=transforms.Compose([
+                    transforms.ToTensor()
+                ])),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    return loader
+
+def create_validation_loader():
+    loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10('./data', train=False, transform=transforms.Compose([
+                    transforms.ToTensor()
+                ])),
+        batch_size=batch_size, shuffle=False, **kwargs)
+    return loader
+
 if __name__ == '__main__':
     cuda = torch.cuda.is_available()
     print('Using PyTorch version:', torch.__version__, 'CUDA:', cuda)
     batch_size = 32
     kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10('./data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ])),
-        batch_size=batch_size, shuffle=True, **kwargs)
-    validation_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10('./data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor()
-                       ])),
-        batch_size=batch_size, shuffle=False, **kwargs)
+    train_loader = create_train_loader()
+    validation_loader = create_validation_loader()
     pltsize=1
     plt.figure(figsize=(10*pltsize, pltsize))
     show_examples()
@@ -112,4 +121,4 @@ if __name__ == '__main__':
     for epoch in range(1, epochs + 1):
         train(epoch)
         validate(lossv, accv)
-    plot()
+    plot_results()
