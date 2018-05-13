1416dbdaee5e886dc6ccfc7c5c54883383494743
dmkato <dmkato@gmail.com>
2018-05-12 21:11:34 -0700

    PyTorch working with CIFAR10

---
+++ Assignment3/MLP.py
@@ -0,0 +1,111 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(32*32*3, 50)
+        self.fc1_drop = nn.Dropout(0.2)
+        self.fc2 = nn.Linear(50, 50)
+        self.fc2_drop = nn.Dropout(0.2)
+        self.fc3 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc2_drop(x)
+        return F.log_softmax(self.fc3(x))
+
+def train(epoch, log_interval=100):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]))
+
+def validate(loss_vector, accuracy_vector):
+    model.eval()
+    val_loss, correct = 0, 0
+    for data, target in validation_loader:
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        val_loss += F.nll_loss(output, target).data[0]
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+
+    val_loss /= len(validation_loader)
+    loss_vector.append(val_loss)
+
+    accuracy = 100. * correct / len(validation_loader.dataset)
+    accuracy_vector.append(accuracy)
+
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(validation_loader.dataset), accuracy))
+
+def show_examples():
+    for i in range(10):
+        plt.subplot(1,10,i+1)
+        plt.axis('off')
+        image = X_train[i,:,:,:].numpy()
+        plt.imshow(image.reshape(3,32,32).transpose(1,2,0))
+        plt.title('Class: '+str(y_train[i]))
+
+def plot():
+    plt.figure(figsize=(5,3))
+    plt.plot(np.arange(1,epochs+1), lossv)
+    plt.title('validation loss')
+
+    plt.figure(figsize=(5,3))
+    plt.plot(np.arange(1,epochs+1), accv)
+    plt.title('validation accuracy');
+    plt.show()
+
+if __name__ == '__main__':
+    cuda = torch.cuda.is_available()
+    print('Using PyTorch version:', torch.__version__, 'CUDA:', cuda)
+    batch_size = 32
+    kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ])),
+        batch_size=batch_size, shuffle=True, **kwargs)
+    validation_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10('./data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor()
+                       ])),
+        batch_size=batch_size, shuffle=False, **kwargs)
+    pltsize=1
+    plt.figure(figsize=(10*pltsize, pltsize))
+    show_examples()
+    model = Net()
+    if cuda:
+        model.cuda()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    print(model)
+    epochs = 2
+    lossv, accv = [], []
+    for epoch in range(1, epochs + 1):
+        train(epoch)
+        validate(lossv, accv)
+    plot()
