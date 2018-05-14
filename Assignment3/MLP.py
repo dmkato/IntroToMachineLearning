4b9effbf7fc88ae02e7563e61d1346e5e373c023
dmkato <dmkato@gmail.com>
2018-05-14 14:25:24 -0700

    Bleh

--- Assignment3/MLP.py
+++ Assignment3/MLP.py
@@ -10,19 +10,15 @@ import matplotlib.pyplot as plt
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(32*32*3, 50)
+        self.fc1 = nn.Linear(32*32*3, 100)
         self.fc1_drop = nn.Dropout(0.2)
-        self.fc2 = nn.Linear(50, 50)
-        self.fc2_drop = nn.Dropout(0.2)
-        self.fc3 = nn.Linear(50, 10)
+        self.fc2 = nn.Linear(100, 10)
 
     def forward(self, x):
         x = x.view(-1, 32*32*3)
-        x = F.relu(self.fc1(x))
+        x = F.sigmoid(self.fc1(x))
         x = self.fc1_drop(x)
-        x = F.relu(self.fc2(x))
-        x = self.fc2_drop(x)
-        return F.log_softmax(self.fc3(x))
+        return F.log_softmax(self.fc2(x))
 
 def train(epoch, log_interval=100):
     model.train()
