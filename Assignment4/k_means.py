b8f74cf223c79ea676b270b00bd64ff8726cd9c6
dmkato <dmkato@gmail.com>
2018-05-21 22:17:38 -0700

    Part 1 Code Written

--- Assignment4/k_means.py
+++ Assignment4/k_means.py
@@ -11,21 +11,13 @@ def get_data(filepath):
     data_set = [np.array(l, dtype=float) for l in lines]
     return np.array(data_set)
 
-def show_first_image(data):
+def show_image(x):
     """
     Displays the first image in the data set
     USES MATPLOTLIB
     """
-    print("done")
-    plt.imshow(data[0].reshape(28, 28), cmap=plt.cm.Greys)
+    plt.imshow(x.reshape(28, 28), cmap=plt.cm.Greys)
     plt.show()
-
-def get_k_random_datapoints(data, k):
-    """
-    Returns k random datapoints
-    """
-    idxs = np.random.choice(len(data), k)
-    return [data[i] for i in idxs]
 
 def add_x_to_nearest_cluster(x, clusters, means):
     """
@@ -54,19 +46,75 @@ def diffs_above_threshold(means, p_means, threshold):
             return True
     return False
 
+def get_k_random_datapoints(data, k):
+    """
+    Returns k random datapoints
+    """
+    idxs = np.random.choice(len(data), k)
+    return [data[i] for i in idxs]
+
+def get_SSE(clusters, means):
+    SSE = 0
+    for m, c in zip(means, clusters):
+        ssum = sum([la.norm(x - m) ** 2 for x in c])
+        SSE += ssum
+    return SSE
+
 def k_means(data, k):
     """
     Returns k disjoint clusters and their means
     """
     means = get_k_random_datapoints(data, k)
-    clusters = [[m] for m in means]
     p_means = [np.zeros(len(means[0])) for m in means]
-    while diffs_above_threshold(means, p_means, threshold=2):
+    SSEs = []
+    while diffs_above_threshold(means, p_means, threshold=0.0001):
+        clusters = [[m] for m in means]
         for x in data:
             add_x_to_nearest_cluster(x, clusters, means)
         p_means = means
         means = update_means(clusters)
+        SSEs += [get_SSE(clusters, means)]
+    return means, SSEs
+
+def show_means(data, k):
+    means, SSE = k_means(data, k=k)
+    fig = plt.figure(figsize=(8, 8))
+    columns = len(means)
+    for i, mean in enumerate(means):
+        img = mean.reshape(28, 28)
+        fig.add_subplot(1, columns, i+1)
+        plt.imshow(img, cmap=plt.cm.Greys)
+
+def plot_SSE(data, k):
+    print("Plotting SSE")
+    means, SSEs = k_means(data, k=k)
+    x_ax = list(range(len(SSEs)))
+    plt.figure(figsize=(5,3))
+    plt.plot(x_ax, SSEs, 'r')
+    plt.title("SSE")
+    plt.xlabel("Iteration")
+    plt.ylabel("SSE")
+
+def plot_model_selection(SSEs, k_max):
+    ks = list(range(2, k_max+1))
+    plt.figure(figsize=(5,3))
+    plt.plot(ks, SSEs, 'r')
+    plt.title("Model Selection")
+    plt.xlabel("k")
+    plt.ylabel("SSE")
+
+def model_selection(data, k_max):
+    results = []
+    for i in range(2, k_max+1):
+        print(i)
+        k_results = [k_means(data, k=i) for c in range(10)]
+        SSEs = [r[1][0] for r in k_results]
+        results += [min(SSEs)]
+    plot_model_selection(results, k_max)
 
 if __name__ == "__main__":
     data = get_data("./unsupervised.txt")
-    k_means(data, k=2)
+    plot_SSE(data, k=2)
+    model_selection(data, k_max=10)
+    show_means(data, k=10)
+    plt.show()
